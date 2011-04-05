01fa62e46b097185853ee6e0a6ecb181f2f4615b
Brian Kiefer <brian.kiefer@gmail.com>
2011-04-05 18:25:09 -0400

    Initial commit

---
+++ src/meal_planner/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls.defaults import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'meal_planner.views.home', name='home'),
+    # url(r'^meal_planner/', include('meal_planner.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
